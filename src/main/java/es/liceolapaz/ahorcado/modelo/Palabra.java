fdcb85b4d665fa643ec2bc29de88a56408db1e95
Francisco Vazquez <fvz2001@icloud.com>
2026-02-09 12:13:33 +0100

    Cracion archivo de configuracion hibernate y clase palabra

---
+++ src/main/java/es/liceolapaz/ahorcado/modelo/Palabra.java
@@ -0,0 +1,26 @@
+package es.liceolapaz.ahorcado.modelo;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "palabras")
+public class Palabra {
+
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "palabra", length = 15, nullable = false)
+    private String palabra;
+    public Palabra() {}
+
+    public Palabra(Long id, String palabra) {
+        this.id = id;
+        this.palabra = palabra;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getPalabra() { return palabra; }
+    public void setPalabra(String palabra) { this.palabra = palabra; }
+}
